dc4f8a9b9160988b58967df15ed48896b19a1192
Carlos Matos <caam1406@gmail.com>
2022-06-14 19:04:53 -0300

    feat: added controller players

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './modules/players/players.module';
+import { PlayersController } from './controllers/players/players.controller';
 
 @Module({
   imports: [PlayersModule],
-  controllers: [],
+  controllers: [PlayersController],
   providers: [],
 })
 export class AppModule {}
